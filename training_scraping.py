7cb956732afb51dc03f03ffd5404ee33970f88a6
PeterSzasz <PeterSzasz@users.noreply.github.com>
2022-09-03 18:01:36 +0200

    mail notofocation added \w mailjet

--- training_scraping.py
+++ training_scraping.py
@@ -12,6 +12,8 @@ FORMAT = '%(asctime)s [%(levelname)s]:  %(message)s'
 logging.basicConfig(format=FORMAT, filename=log_path, encoding="utf8")
 log = logging.getLogger("scraping")
 log.setLevel(10)
+mail_content = "Hi\n\nToday's results:\n\n"
+mail_subject = "Codecool results!"
 
 # get content
 raw_page = requests.get(URL, timeout=5)
@@ -19,8 +21,6 @@ page = BeautifulSoup(raw_page.content, "html.parser")
 
 # parse
 results = [match.start() for match in re.finditer("A jelentkezés határideje:|Jelentkezési határidő:", str(raw_page.content, "utf8"))]
-print(str(raw_page.content, "utf8"))
-print(results)
 
 # log
 log.info("---------- Today's results: ----------")
@@ -28,5 +28,41 @@ for result in results:
     result_end_1 = str(raw_page.content, "utf8").find(" />", result)
     result_end_2 = str(raw_page.content, "utf8").find("</p>", result)
     result_end = result_end_1 if result_end_1 < result_end_2 else result_end_2
-    log.info("from "+str(result)+": "+str(raw_page.content, "utf8")[result:result_end])
+    for_log = "from "+str(result)+": "+str(raw_page.content, "utf8")[result:result_end]
+    log.info(for_log)
+    mail_content += for_log+"\n"
 log.info("logfile size: "+ str(os.path.getsize(log_path)))
+
+# check update
+matches = re.findall("november|október|december", str(raw_page.content, "utf8").lower())
+if matches != []:
+    for match in matches:
+        mail_subject = "!!!! CODECOOL UPDATED to "+str(match)+" !!!!"
+
+    # mail
+    from mailjet_rest import Client
+    api_key = ''
+    api_secret = ''
+    mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+    data = {
+    'Messages': [
+        {
+        "From": {
+            "Email": "",
+            "Name": ""
+        },
+        "To": [
+            {
+            "Email": "",
+            "Name": ""
+            }
+        ],
+        "Subject": mail_subject,
+        "TextPart": mail_content,
+        "HTMLPart": mail_content.replace("\n","<br>")+"<br>More info at <a href='"+URL+"'>CodeCool</a>!",
+        "CustomID": ""
+        }
+    ]
+    }
+    result = mailjet.send.create(data=data)
+
